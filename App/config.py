80fb02174e4c0ac4f7ea2df49af54b790be3467c
LesserKnown0ne <ryan.harris1@my.uwi.edu>
2022-04-24 00:24:50 +0000

    changed config

--- App/config.py
+++ App/config.py
@@ -1,4 +1,6 @@
-SQLALCHEMY_DATABASE_URI = "sqlite:///temp-database.db"
+SQLALCHEMY_DATABASE_URI = "postgres://hjkxtcfgeoorto:9cad0fb3272e1ec4f1d92e725265e5f94745f0eaabc3e643407012176abc0780@ec2-54-80-122-11.compute-1.amazonaws.com:5432/d3dnglm7per98a"
 SECRET_KEY = "secret key"
 JWT_EXPIRATION_DELTA = 7
 ENV = "PRODUCTION"
+SQLDBLITE = False
+DBURI = "postgres://hjkxtcfgeoorto:9cad0fb3272e1ec4f1d92e725265e5f94745f0eaabc3e643407012176abc0780@ec2-54-80-122-11.compute-1.amazonaws.com:5432/d3dnglm7per98a"
