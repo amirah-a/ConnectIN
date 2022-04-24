0297f23332a1c1c3882f8210ddcd599e3c5667c7
LesserKnown0ne <ryan.harris1@my.uwi.edu>
2022-04-24 00:57:40 +0000

    co

--- App/models/user.py
+++ App/models/user.py
@@ -10,7 +10,7 @@ class User(UserMixin, db.Model):
     email =  db.Column(db.String, nullable=False, unique=True)
     password = db.Column(db.String(120), nullable=False)
     year = db.Column(db.Integer, nullable= True)
-    degree = db.Column(db.Integer, nullable= True)
+    degree = db.Column(db.String, nullable= True)
     department = db.Column(db.String, nullable=True)
     faculty = db.Column(db.String, nullable=True)
     experience = db.Column(db.String, nullable=True)
