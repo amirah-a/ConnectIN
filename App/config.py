4299d47ed68b574e3b1c7086e914792eff5791f5
LesserKnown0ne <ryan.harris1@my.uwi.edu>
2022-04-24 00:41:33 +0000

    changed config4

--- App/config.py
+++ App/config.py
@@ -2,4 +2,3 @@ SQLALCHEMY_DATABASE_URI = "postgresql://hjkxtcfgeoorto:9cad0fb3272e1ec4f1d92e725
 SECRET_KEY = "secret key"
 JWT_EXPIRATION_DELTA = 7
 ENV = "PRODUCTION"
-DBURI = "postgres://hjkxtcfgeoorto:9cad0fb3272e1ec4f1d92e725265e5f94745f0eaabc3e643407012176abc0780@ec2-54-80-122-11.compute-1.amazonaws.com:5432/d3dnglm7per98a"
